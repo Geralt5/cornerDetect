495ee0c29e28af31d57609aa6ef56d3e11f63793
Geralt5 <57083099+Geralt5@users.noreply.github.com>
2024-04-17 19:42:23 -0400

    openmp

--- FindCorners.cpp
+++ FindCorners.cpp
@@ -496,14 +496,14 @@ void FindCorners::detectCorners(Mat &Src, vector<Point> &resultCornors, float sc
 	normalize(gray, imageNorm, 0, 1, cv::NORM_MINMAX, CV_32F);//对灰度图进行归一化
 
 	Mat imgCorners = Mat::zeros(imageNorm.size(), CV_32F);//卷积核得出的点
-	vector<std::thread> threads;
-	//OpenMP: omp_set_num_threads(num_threads);
-	//OpenMP: #pragma omp parallel for
+	//vector<std::thread> threads;
+	omp_set_num_threads(num_threads);
+	#pragma omp parallel for
 	//OpenCV: FindCorners cornerDetector;
 	//OpenCV: Parallel_process processor(imageNorm, imgCorners, templateProps, radius, cornerDetector);
     //OpenCV: cv::parallel_for_(cv::Range(0, 6), processor);
-	//for (int i = 0; i < 6; i++)
-	auto processKernel = [&](int i)
+	for (int i = 0; i < 6; i++)
+	//auto processKernel = [&](int i)
 	{
 		//按照论文步骤，第一步：用卷积核进行卷积的方式找出可能是棋盘格角点的点
 		Mat kernelA1, kernelB1, kernelC1, kernelD1;
@@ -537,15 +537,15 @@ void FindCorners::detectCorners(Mat &Src, vector<Point> &resultCornors, float sc
 
 		getMax(imgCorners, imgCorner1, imgCorners);
 		getMax(imgCorners, imgCorner2, imgCorners);
-	};
+	}//;
 	
-	for (int i = 0; i < num_threads; i++) {
-        threads.emplace_back(processKernel, i);
-    }
+	// for (int i = 0; i < num_threads; i++) {
+    //     threads.emplace_back(processKernel, i);
+    // }
 
-   for (auto& t : threads) {
-        t.join();
-    }
+    // for (auto& t : threads) {
+    //     t.join();
+    // }
 
 	nonMaximumSuppression(imgCorners, cornerPoints, 0.01, 5, 3);//1.5 非极大值抑制算法进行过滤，获取棋盘格角点初步结果
 
