3dd120728d99db9788de8a6a219912d35bfa938a
Geralt5 <57083099+Geralt5@users.noreply.github.com>
2024-04-17 16:28:25 -0400

    head file change

--- FindCorners.h
+++ FindCorners.h
@@ -23,14 +23,15 @@ public:
 
 public:
 	void detectCorners(Mat &Src, vector<Point> &resultCornors,float scoreThreshold, int num_threads);
-
-private:
-	//正态分布
-	float normpdf(float dist, float mu, float sigma);
 	//获取最小值
 	void getMin(Mat src1, Mat src2, Mat &dst);
 	//获取最大值
-	void getMax(Mat src1, Mat src2, Mat &dst);
+	void getMax(Mat src1, Mat src2, Mat &dst);	
+	//生成核
+	void createkernel(float angle1, float angle2, int kernelSize, Mat &kernelA, Mat &kernelB, Mat &kernelC, Mat &kernelD);
+private:
+	//正态分布
+	float normpdf(float dist, float mu, float sigma);
 	//获取梯度角度和权重
 	void getImageAngleAndWeight(Mat img, Mat &imgDu, Mat &imgDv, Mat &imgAngle, Mat &imgWeight);
 	//estimate edge orientations
@@ -43,8 +44,6 @@ private:
 	void cornerCorrelationScore(Mat img, Mat imgWeight, vector<Point2f> cornersEdge, float &score);
 	//亚像素精度找角点
 	void refineCorners(vector<Point> &cornors, Mat imgDu, Mat imgDv, Mat imgAngle, Mat imgWeight, float radius);
-	//生成核
-	void createkernel(float angle1, float angle2, int kernelSize, Mat &kernelA, Mat &kernelB, Mat &kernelC, Mat &kernelD);
 	//非极大值抑制
 	void nonMaximumSuppression(Mat& inputCorners, vector<Point>& outputCorners, float threshold, int margin, int patchSize);
 
